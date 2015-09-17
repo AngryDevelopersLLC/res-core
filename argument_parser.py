3c41d9be18bf2d7f431cce31b7473246d44ce0c2
Vadim Markovtsev <gmarkhor@gmail.com>
2015-09-17 14:15:58 +0300

    Change argument parsing

--- argument_parser.py
+++ argument_parser.py
@@ -7,12 +7,15 @@ import inspect
 import sys
 
 
-def get_argument_parser(description):
-    return argparse.ArgumentParser(description=description)
+ARGPARSE_DESCRIPTION = "Change res.core.argument_parser.ARGPARSE_DESCRIPTION!"
+
+def get_argument_parser():
+    return argparse.ArgumentParser(description=ARGPARSE_DESCRIPTION)
 
 
 def gather_parsers(parser, packages=tuple()):
-    packages = (sys.modules[__name__].__package__,) + packages
+    packages = (sys.modules[sys.modules[__name__].__package__],) + \
+        tuple(packages)
     for pkg in packages:
         for _, mod in inspect.getmembers(pkg, inspect.ismodule):
             initfn = getattr(mod, "init_argument_parser", None)
