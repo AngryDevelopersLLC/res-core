da4981e32588ddc594988d3902f8c6d184b10b58
Vadim Markovtsev <gmarkhor@gmail.com>
2015-11-11 10:46:16 +0300

    Add Debian packaging
    Change-Id: Icc5e4243f657aa178efa41797c12d5f9363256c6

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import os
+
+
+def parse_requirements():
+    path = os.path.join(os.path.dirname(__file__), "res", "core",
+                        "requirements", "base.txt")
+    reqs = []
+    with open(path, "r") as fin:
+        for r in fin.read().split("\n"):
+            r = r.strip()
+            if r.startswith("#") or not r:
+                continue
+            if r.startswith("git+"):
+                print("Warning: git dependencies cannot be used in setuptools "
+                      "(%s)" % r)
+                continue
+            if not r.startswith("-r"):
+                reqs.append(r)
+    return reqs
+
+
+setup(
+    name="res-core",
+    description="RESystem common service package",
+    version="1.0.0",
+    license="Proprietary",
+    author="Angry Developers",
+    author_email="gmarkhor@gmail.com",
+    url="http://dev.res-it.net/gerrit/#/q/project:res-core",
+    download_url='http://dev.res-it.net/gerrit/#/q/project:res-core',
+    packages=["res.core"],
+    install_requires=parse_requirements(),
+    package_data={"": ["res/core/requirements/base.txt"]},
+    classifiers=[
+        "Development Status :: 5 - Stable",
+        "Environment :: Console",
+        "License :: Proprietary",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4"
+    ]
+)
